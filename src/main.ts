bf7f7c0e0ebcc52e2bdb619b02b1163f31463493
Faridah Salami <f.salami@outlook.com>
2024-04-08 18:22:37 +0100

    for loop works but generates all clues at once, try switch case next

--- src/main.ts
+++ src/main.ts
@@ -19,17 +19,24 @@ if(!begin || !clueCard|| !guessInput || !submit){
     throw new Error ("Issue with selector")
 }
 
-const random = Words[Math.floor(Math.random()*Words.length)]
+//The word the user is trying to guess
+const random = Words[Math.floor(Math.random()*Words.length)] 
 
+//The function that controls the commencement of the game. Will always start with the first clue in the clues array
 const handlStartGame = () => {
     clueCard.innerText = random.clues[0]
 }
 
+//The function that checks the guess of the user against the word. Returns a well done message if guess is correct. Otherwise a try again message if it is not a match
 const handleGuessInput = () => {
     const guessWord = guessInput.value
-    if(guessWord == random.word){
+    if(guessWord === random.word){
         alert("WELL DONE")
     } else{
+        clueCard.innerText = random.clues[0] + "\n"
+        for(let i = 1; i < random.clues.length; i++){
+            clueCard.innerText += random.clues[i] + "\n";
+        }
         alert ("Try again")
     }
 }
